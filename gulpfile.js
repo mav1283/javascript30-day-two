4ba82d39d38e177ab90897c74a96a4c4ab4b69b6
Dani Paolo S. Dullano <pdullano@gmail.com>
2018-10-03 22:36:57 +0800

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 let styleSource = './src/scss/style.scss';
 let styleDestination = './build/css/';
@@ -8,9 +10,15 @@ let styleDestination = './build/css/';
 gulp.task('styles',function(){
     gulp.src(styleSource)
         .pipe(sass({
-            errorLogToConsole:true
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
         }))
         .on('error', console.error.bind(console))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(rename({suffix:'.min'}))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(styleDestination));
 });
