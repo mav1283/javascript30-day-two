85d2952b3d03b6a11092a7632f8d160d073089af
Dani Paolo S. Dullano <pdullano@gmail.com>
2018-10-03 21:48:45 +0800

    updating stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
 let styleSource = './src/scss/style.scss';
 let styleDestination = './build/css/';
 
 gulp.task('styles',function(){
     gulp.src(styleSource)
+        .pipe(sass({
+            errorLogToConsole:true
+        }))
+        .on('error', console.error.bind(console))
         .pipe(rename({suffix:'.min'}))
         .pipe(gulp.dest(styleDestination));
 });
